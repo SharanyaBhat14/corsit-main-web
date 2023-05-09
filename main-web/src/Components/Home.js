bf602d97ab10d3121bbe74d48e8a62cee42179f1
aman6649 <aman.ag6649@gmail.com>
2023-05-09 10:47:40 +0530

    dukhi aatma

--- main-web/src/Components/Home.js
+++ main-web/src/Components/Home.js
@@ -12,18 +12,18 @@ const Home = () => {
         src={walle}
       />
       <div className=" absolute top-32 left-[55%] max-w-screen-sm -translate-x-10 -translate-y-[10%] mx-auto flex flex-col text-center sm:ml-28 sm:top-36">
-        <p className=" font-bold ml-1 p-2 text-2xl">Learn with us</p>
-        <h1 className="md:text-5xl sm:text-4xl text-3xl py-4 font-bold  md:ml-11">
+        <p className=" font-bold ml-1 p-2 text-lg">Learn with us</p>
+        <h1 className="md:text-5xl sm:text-4xl text-2xl py-4 font-bold  md:ml-11">
           Grow with us
         </h1>
       </div>
-      <div className=" absolute top-44 left-[29%] sm:left-[40%] max-w-screen-sm -translate-x-10 -translate-y-[10%] mx-auto flex flex-col text-center sm:ml-44 sm:pt-14">
+      <div className=" absolute top-[182px] left-[29%] sm:left-[40%] max-w-screen-sm -translate-x-10 -translate-y-[10%] mx-auto flex flex-col text-center sm:ml-44 sm:pt-14">
         <div className=" px-4 ml-[4.3rem] mt-20 sm:mt-14 flex items-center">
           <p className="md:text-5xl sm:text-4xl text-lg font-bold pt-0 ">
             Learn
           </p>
           <Typed
-            className="md:text-5xl sm:text-4xl text-xl font-bold whitespace-nowrap md:pl-4 pl-1 "
+            className="md:text-5xl sm:text-4xl text-lg font-bold whitespace-nowrap md:pl-4 pl-1 "
             strings={[
               "Robotics",
               "Web Development",
