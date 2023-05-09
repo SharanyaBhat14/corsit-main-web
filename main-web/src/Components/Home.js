1fc03d7161c0cffcd7b12145022ba7eceea1cadd
aman6649 <aman.ag6649@gmail.com>
2023-05-09 11:43:42 +0530

    home page done

--- main-web/src/Components/Home.js
+++ main-web/src/Components/Home.js
@@ -17,13 +17,13 @@ const Home = () => {
           Grow with us
         </h1>
       </div>
-      <div className=" absolute top-[182px] left-[29%] sm:left-[40%] max-w-screen-sm -translate-x-10 -translate-y-[10%] mx-auto flex flex-col text-center sm:ml-44 sm:pt-14">
-        <div className=" px-4 ml-[4.3rem] mt-20 sm:mt-14 flex items-center">
+      <div className=" absolute top-44 left-[29%] sm:left-[40%] max-w-screen-sm -translate-x-10 -translate-y-[10%] mx-auto flex flex-col text-center sm:ml-44 sm:pt-14">
+        <div className=" px-4 ml-[4.3rem] mt-16 sm:mt-14 flex items-center">
           <p className="md:text-5xl sm:text-4xl text-lg font-bold pt-0 ">
             Learn
           </p>
           <Typed
-            className="md:text-5xl sm:text-4xl text-lg font-bold whitespace-nowrap md:pl-4 pl-1 "
+            className="md:text-5xl sm:text-4xl text-base font-bold whitespace-nowrap md:pl-4 pl-1 "
             strings={[
               "Robotics",
               "Web Development",
